834d49ec52acd37d41ed676eaedaa37aee715b7a
CosmasNsikak2004 <cosmasnsikakabasi@gmail.com>
2023-08-30 03:07:18 -0700

    init

--- script.js
+++ script.js
@@ -38,27 +38,27 @@ $('.testimonials-container').owlCarousel({
 })
 
 // On load Animation
-gsap.fromTo(
-    ".loading-page",
-    { opacity: 1 },
-    {
-      opacity: 0,
-      display: "none",
-      duration: 1.5,
-      delay: 3.5,
-    }
-  );
+// gsap.fromTo(
+//     ".loading-page",
+//     { opacity: 1 },
+//     {
+//       opacity: 0,
+//       display: "none",
+//       duration: 1.5,
+//       delay: 3.5,
+//     }
+//   );
   
-  // gsap.fromTo(
-  //   ".logo-name",
-  //   {
-  //     y: 50,
-  //     opacity: 0,
-  //   },
-  //   {
-  //     y: 0,
-  //     opacity: 1,
-  //     duration: 2,
-  //     delay: 0.5,
-  //   }
-  // );
+//   gsap.fromTo(
+//     ".logo-name",
+//     {
+//       y: 50,
+//       opacity: 0,
+//     },
+//     {
+//       y: 0,
+//       opacity: 1,
+//       duration: 2,
+//       delay: 0.5,
+//     }
+//   );
